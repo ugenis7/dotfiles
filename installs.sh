60058a71b0477b9d4d5aa5faba055d218889d2d3
Ulises Genis <ulises.genis@gmail.com>
2018-09-08 19:03:19 -0500

    changed the order of list

--- installs.sh
+++ installs.sh
@@ -5,88 +5,29 @@ sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo cp /home/ugenis/.zshrc ~
 
-sudo dnf install -y
+# Necessary
+sudo dnf install -y baobab bleachbit deja-dup dnf-utils exfat-utils fuse-exfat gvfs-mtp gnome-shell-extension-pomodoro gnome-tweaks gnucash gparted gvim keepass kernel-core kernel-devel kernel-modules kernel-modules-extra mediawriter nextcloud-client R thunar unrar vim zsh
 
-Important:
-baobab
-bleachbit
-deja-dup
-dnf-utils
-exfat-utils
-fuse-exfat
-gvfs-mtp
-gnome-shell-extension-pomodoro
-gnome-tweaks
-gnucash
-gparted
-gvim
-keepass
-kernel-core
-kernel-devel
-kernel-modules
-kernel-modules-extra
-mediawriter
-* neovim copr
-nextcloud-client
-R
-thunar
-unrar
-vim
-zsh
+# Copr:
+sudo dnf copr enable dperson/neovim
+sudo dnf install neovim python3-neovim
 
+# Normal repositories
+sudo dnf install adobe-source-\* google-droid-sans-fonts google-roboto-\* calibre clementine ffmpeg gimp gpodder mediainfo picard puddletag soundkonverter xsane youtube-dl zathura filezilla lftp liferea lyx nfoview pandoc powertop remmina remmina-devel remmina-plugins-spice tmux texlive-\* mpv corebird tlp tlp-rdw
 
-Fonts:
-adobe-source-\*
-google-droid-sans-fonts
-google-roboto-\*
-
-Multimedia:
-* Audacity
+# Flatpak
 * bookworm
-calibre
-clementine
 * cozy
-ffmpeg
 * frogr
-gimp
-gpodder
-mediainfo
-* mpv
-picard
-puddletag
-soundkonverter
-xsane
-* vlc
-youtube-dl
-zathura
-
-Internet:
-flatpak install corebird
-filezilla
-lftp
-liferea
 * skypeforlinux
-thunderbird
-* Plex
-* google-chrome
-* hexchat or polari
+* hexchat
 * Signal
 * Telegram
-
-Development:
-lyx
 * mendeley
-nfoview
-pandoc
-powertop
-remmina
-remmina-devel
-remmina-plugins-spice
-* rstudio
-texlive-\*
-tlp
-tlp-rdw
-tlp-release
-tmux
+* vlc
+* google-chrome
 
+# Other repositories
 * Virtualbox
+* Plex
+* rstudio
